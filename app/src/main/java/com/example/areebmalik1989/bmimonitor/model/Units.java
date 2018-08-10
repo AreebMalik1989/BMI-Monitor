f3cfbe22ab69caa31829779a6eecdef4928dda08
Unknown <areeb.malik1989@gmail.com>
2018-08-10 20:23:20 +0530

    Added Units.java

---
+++ app/src/main/java/com/example/areebmalik1989/bmimonitor/model/Units.java
@@ -0,0 +1,16 @@
+package com.example.areebmalik1989.bmimonitor.model;
+
+public class Units {
+
+    public enum WeightUnit {
+        KG,
+        LBS
+    }
+
+    public enum LengthUnit {
+        METER,
+        CENTIMETER,
+        FEET_INCHES,
+        INCHES
+    }
+}
