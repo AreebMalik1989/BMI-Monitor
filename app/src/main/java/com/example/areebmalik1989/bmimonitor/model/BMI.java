c23c9d51ec27b95ade0a765acf9321193e270e49
Unknown <areeb.malik1989@gmail.com>
2018-08-10 20:22:32 +0530

    Added BMI.java

---
+++ app/src/main/java/com/example/areebmalik1989/bmimonitor/model/BMI.java
@@ -0,0 +1,13 @@
+package com.example.areebmalik1989.bmimonitor.model;
+
+public enum BMI {
+    VERY_SEVERELY_UNDERWEIGHT,
+    SEVERELY_UNDERWEIGHT,
+    UNDERWEIGHT,
+    NORMAL,
+    OVER_WEIGHT,
+    OBESE_I,
+    OBESE_II,
+    OBESE_III,
+    UNKNOWN
+}
