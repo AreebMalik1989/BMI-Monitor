bc28011113262fbd3a4a2f7c5601fe4e07a080fe
Unknown <areeb.malik1989@gmail.com>
2018-08-11 22:37:22 +0530

    Added MainActivityTest.java

---
+++ app/src/test/java/com/example/areebmalik1989/bmimonitor/view/MainActivityTest.java
@@ -0,0 +1,29 @@
+package com.example.areebmalik1989.bmimonitor.view;
+
+import com.example.areebmalik1989.bmimonitor.model.Person;
+
+import org.junit.After;
+import org.junit.Before;
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+
+public class MainActivityTest {
+
+    IMainActivityView mainActivity;
+
+    @Before
+    public void setUp() throws Exception {
+        mainActivity = new MainActivity();
+    }
+
+    @After
+    public void tearDown() throws Exception {
+    }
+
+    @Test
+    public void shouldCollectData(){
+    }
+
+
+}
