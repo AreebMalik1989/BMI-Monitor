d68440207f033a89cd92ea47674db58e5c77f289
Unknown <areeb.malik1989@gmail.com>
2018-08-18 12:36:45 +0530

    Optimized import

--- app/src/main/java/com/example/areebmalik1989/bmimonitor/model/calculator/IBmiCalculator.java
+++ app/src/main/java/com/example/areebmalik1989/bmimonitor/model/calculator/IBmiCalculator.java
@@ -1,7 +1,6 @@
 package com.example.areebmalik1989.bmimonitor.model.calculator;
 
 import com.example.areebmalik1989.bmimonitor.model.BMI;
-import com.example.areebmalik1989.bmimonitor.model.Person;
 
 public interface IBmiCalculator {
     double calculateBmi(double height, double weight);
