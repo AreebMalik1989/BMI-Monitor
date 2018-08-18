db3ae18f8596551f0fb2b640d92bc9162f5560f9
Unknown <areeb.malik1989@gmail.com>
2018-08-18 12:35:45 +0530

    Updated BMI model

--- app/src/main/java/com/example/areebmalik1989/bmimonitor/model/BMI.java
+++ app/src/main/java/com/example/areebmalik1989/bmimonitor/model/BMI.java
@@ -1,13 +1,25 @@
 package com.example.areebmalik1989.bmimonitor.model;
 
 public enum BMI {
-    VERY_SEVERELY_UNDERWEIGHT,
-    SEVERELY_UNDERWEIGHT,
-    UNDERWEIGHT,
-    NORMAL,
-    OVER_WEIGHT,
-    OBESE_I,
-    OBESE_II,
-    OBESE_III,
-    UNKNOWN
+
+    VERY_SEVERELY_UNDERWEIGHT("Very Severely Underweight"),
+    SEVERELY_UNDERWEIGHT("Severely Underweight"),
+    UNDERWEIGHT("Underweight"),
+    NORMAL("Normal"),
+    OVER_WEIGHT("Overweight"),
+    OBESE_I("Obese I"),
+    OBESE_II("Obese II"),
+    OBESE_III("Obese III"),
+    UNKNOWN("Unknown");
+
+    private String bmi;
+
+    BMI(String bmi){
+        this.bmi = bmi;
+    }
+
+    @Override
+    public String toString() {
+        return bmi;
+    }
 }
