997afa8f4a9cd21d8733cfcea8b40b99b55a61f9
Unknown <areeb.malik1989@gmail.com>
2018-08-19 19:36:17 +0530

    Added BmiFragmentTest.java

---
+++ app/src/test/java/com/example/areebmalik1989/bmimonitor/view/fragment/bmiFragment/BmiFragmentTest.java
@@ -0,0 +1,58 @@
+package com.example.areebmalik1989.bmimonitor.view.fragment.bmiFragment;
+
+import android.widget.ArrayAdapter;
+import android.widget.EditText;
+import android.widget.Spinner;
+import android.widget.TextView;
+
+import com.example.areebmalik1989.bmimonitor.model.Height;
+import com.example.areebmalik1989.bmimonitor.model.Units;
+import com.example.areebmalik1989.bmimonitor.model.Weight;
+import com.example.areebmalik1989.bmimonitor.model.calculator.BmiCalculator;
+import com.example.areebmalik1989.bmimonitor.model.calculator.IBmiCalculator;
+import com.example.areebmalik1989.bmimonitor.model.converter.Converter;
+import com.example.areebmalik1989.bmimonitor.model.converter.IConverter;
+import com.example.areebmalik1989.bmimonitor.view.fragment.BmiFragment;
+import com.example.areebmalik1989.bmimonitor.view.fragment.IBmiFragment;
+
+import junit.framework.Assert;
+
+import org.junit.Before;
+import org.junit.Test;
+import org.mockito.Mock;
+import org.mockito.Mockito;
+import org.w3c.dom.Text;
+
+public class BmiFragmentTest {
+
+    IBmiCalculator bmiCalculator;
+    IConverter converter;
+
+    TextView bmiOutput;
+    EditText weightEditText, heightEditText;
+    Spinner weightSpinner, heightSpinner;
+
+    IBmiFragment bmiFragment;
+
+    @Before
+    public void setUp() throws  Exception {
+
+        this.bmiCalculator = new BmiCalculator();
+        this.converter = new Converter();
+        this.bmiOutput = Mockito.mock(TextView.class);
+        this.weightEditText = Mockito.mock(EditText.class);
+        this.heightEditText = Mockito.mock(EditText.class);
+        this.weightSpinner = Mockito.mock(Spinner.class);
+        this.heightSpinner = Mockito.mock(Spinner.class);
+
+        this.bmiFragment = new BmiFragment();
+    }
+
+    @Test
+    public void shouldSetWeightSpinner(){
+    }
+
+    @Test
+    public void shouldSetHeightSpinner(){}
+
+}
