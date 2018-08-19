0eede3c90bf4d97d2646c48de48524d5760faaff
Unknown <areeb.malik1989@gmail.com>
2018-08-19 19:38:12 +0530

    Added BmiManagerTest.java

---
+++ app/src/test/java/com/example/areebmalik1989/bmimonitor/model/util/BmiManagerTest.java
@@ -0,0 +1,42 @@
+package com.example.areebmalik1989.bmimonitor.model.util;
+
+import android.os.Bundle;
+
+import com.example.areebmalik1989.bmimonitor.model.BMI;
+import com.example.areebmalik1989.bmimonitor.model.Units;
+
+import junit.framework.Assert;
+
+import org.junit.After;
+import org.junit.Before;
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+
+public class BmiManagerTest {
+
+    IBmiManager bmiManager;
+
+    @Before
+    public void setUp() throws Exception {
+        bmiManager = new BmiManager();
+    }
+
+    @After
+    public void tearDown() throws Exception {
+    }
+
+    @Test
+    public void test(){
+        bmiManager.collectBmiData(80.0, Units.WeightUnit.KG.toString(),
+                2.0, Units.LengthUnit.METER.toString());
+
+        Bundle bundle = bmiManager.giveBmiResult();
+
+        double bmi = bundle.getDouble(IBmiManager.BMI);
+        String bmiTranslation = bundle.getString(IBmiManager.BMI_TRANSLATION);
+
+        Assert.assertEquals(20.0, bmi);
+        Assert.assertEquals(BMI.NORMAL.toString(), bmiTranslation);
+    }
+}
