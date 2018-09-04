35117c8ce69478bfc46dc69fde93b9f7c912c6e2
Unknown <areeb.malik1989@gmail.com>
2018-09-04 23:00:24 +0530

    Added string in bmi interface

--- app/src/main/java/com/example/areebmalik1989/bmimonitor/view/fragment/IBmiFragment.java
+++ app/src/main/java/com/example/areebmalik1989/bmimonitor/view/fragment/IBmiFragment.java
@@ -2,6 +2,8 @@ package com.example.areebmalik1989.bmimonitor.view.fragment;
 
 public interface IBmiFragment {
 
+    String BMI_VALUE = "BMI_VALUE";
+
     void collectData();
     void showResult();
     
