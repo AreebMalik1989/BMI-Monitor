351b0ebb53c607de4974efb3eb93beeea1f9b60c
Unknown <areeb.malik1989@gmail.com>
2018-09-11 07:14:07 +0530

    Updated to return recordbox instead of recordstore

--- app/src/main/java/com/example/areebmalik1989/bmimonitor/MainApp.java
+++ app/src/main/java/com/example/areebmalik1989/bmimonitor/MainApp.java
@@ -3,14 +3,17 @@ package com.example.areebmalik1989.bmimonitor;
 import android.app.Application;
 import android.util.Log;
 
+import com.example.areebmalik1989.bmimonitor.repository.BmiRecord;
 import com.example.areebmalik1989.bmimonitor.repository.MyObjectBox;
 
+import io.objectbox.Box;
 import io.objectbox.BoxStore;
 import io.objectbox.android.AndroidObjectBrowser;
 
 public class MainApp extends Application {
 
     private static BoxStore recordStore;
+    private static Box<BmiRecord> bmiRecordBox;
     private static MainApp mainApp;
 
     @Override
@@ -19,6 +22,7 @@ public class MainApp extends Application {
 
         mainApp = this;
         recordStore = MyObjectBox.builder().androidContext(MainApp.this).build();
+        bmiRecordBox = recordStore.boxFor(BmiRecord.class);
 
         if (BuildConfig.DEBUG) {
             new AndroidObjectBrowser(recordStore).start(this);
@@ -31,7 +35,7 @@ public class MainApp extends Application {
         return mainApp;
     }
 
-    public static BoxStore getRecordStore(){
-        return recordStore;
+    public static Box<BmiRecord> getBmiRecordBox(){
+        return bmiRecordBox;
     }
 }
