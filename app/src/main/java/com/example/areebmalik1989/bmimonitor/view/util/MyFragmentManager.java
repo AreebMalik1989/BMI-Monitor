2e76129358c5d070035fff064edac82b4337d07a
Unknown <areeb.malik1989@gmail.com>
2018-09-11 07:16:19 +0530

    Updated fragments

--- app/src/main/java/com/example/areebmalik1989/bmimonitor/view/util/MyFragmentManager.java
+++ app/src/main/java/com/example/areebmalik1989/bmimonitor/view/util/MyFragmentManager.java
@@ -4,7 +4,9 @@ import android.support.v4.app.Fragment;
 import android.support.v4.app.FragmentManager;
 import android.support.v7.app.AppCompatActivity;
 
+import com.example.areebmalik1989.bmimonitor.R;
 import com.example.areebmalik1989.bmimonitor.view.fragment.BmiFragment;
+import com.example.areebmalik1989.bmimonitor.view.fragment.StatsFragment;
 
 /**
  * Created by areeb on 27/7/18.
@@ -15,13 +17,15 @@ public class MyFragmentManager {
     private FragmentManager fragmentManager;
     private int contentFrameId;
 
-    public Fragment bmiFragment;
+    private Fragment bmiFragment;
+    private Fragment statsFragment;
 
     public MyFragmentManager(AppCompatActivity activity, int contentFrameId){
         this.fragmentManager = activity.getSupportFragmentManager();
         this.contentFrameId = contentFrameId;
 
         bmiFragment = new BmiFragment();
+        statsFragment = new StatsFragment();
     }
 
     public void updateFragment(Fragment fragment){
@@ -32,9 +36,17 @@ public class MyFragmentManager {
 
         switch (s){
 
-            case "BMI":
-                this.updateFragment(bmiFragment);
+            case "Calculator":
+                updateFragment(bmiFragment);
+                break;
+
+            case "Stats":
+                updateFragment(statsFragment);
                 break;
         }
+    }
+
+    public Fragment getBmiFragment(){
+        return bmiFragment;
     }
 }
